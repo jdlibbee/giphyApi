b2da0b065a4860398d3e3fb02d8917e2892322bb
jdlibbee <libbeelibbee@yahoo.com>
2018-05-31 18:14:31 -0600

    updated JavaScript

--- assets/java/app.js
+++ assets/java/app.js
@@ -1,7 +1,6 @@
 //have a set of buttons already created. 
 var starter = ["Naruto", "Bleach", "Wolf's Rain", "Sword Art Online", "Princess Mononoke"];
-var still = true;
-var giphInfo;
+var moving = false;
 //user pushes buttons to have a giphy apper. 
 //user can add button with the search and add function in navbar. 
 //User can click on giphy to start or stop it from running. 
@@ -20,6 +19,7 @@ function renderButton() {
 }
 //show giphys
 function displayGiphy() {
+    $('#giphySpot').empty();
     var animes = $(this).attr('data-name');
     console.log(this);
     var queryURL = `https://api.giphy.com/v1/gifs/search?q=${animes}&rating=pg&api_key=vxNW00aofQmE0aXIBDZmewCRmfncbOPd&limit=10`;
@@ -31,30 +31,32 @@ function displayGiphy() {
         for (var i = 0; i < results.data.length; i++) {
             var giphy = `
          <div class="card col-sm-4 bg-info mb-3">
-            <img class="card-img-top" id="giphImage" src="${results.data[i].images.original_still.url} " data-animate="${results.data[i].images.looping.mp4}" data-still="${results.data[i].images.original_still.url}"alt="Anime Giphy">
+            <img class="card-img-top" id="giphImage" src="${results.data[i].images.original_still.url} " data-animated="${results.data[i].images.fixed_height.url}" data-still="${results.data[i].images.original_still.url}" data-state="still">
                 <h5 class="card-title">${results.data[i].title}</h5>
                 <p class="card-text">Rating: ${results.data[i].rating}</p>
         </div>`;
             $('#giphySpot').append(giphy);
-            still = true;
-            giphInfo = results[i];
+            moving = false;
         }
-        console.log('info: ' + giphInfo);
     })
 
 }
 //play or pause giph
-$('#giphySpot').on("click", "#giphImage", function () {
-    var animate = $(this).attr('data-animate');
-    var still = $(this).attr('data-still');
-    if (still = true) {
-        $(this).attr('src', animate);
-        still = false;
+$(document).on("click", "#giphImage", function () {
+    var animated = $(this).data('animated');
+    var still = $(this).data('still');
+    var state = $(this).attr('data-state');
+    console.log('animate: ' + animated);
+    console.log('still: ' + still);
+    if (state == 'still') {
+        $(this).attr('src', animated);
+        $(this).attr('data-state', 'animated');
+        console.log(this);
     } else {
         $(this).attr('src', still);
-        still = true;
+        $(this).attr('data-state', 'still');
+        console.log(this);
     }
-    console.log(this);
 })
 //add new anime button
 $('#addAnime').on('click', function (event) {
